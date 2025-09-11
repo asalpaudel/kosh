1be4768ccd68f3b2c3ec04744c50fd154101ed42
Asal Paudel <paudel.asal00@gmail.com>
2025-09-11 14:23:40 +0545

    initial

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-800">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
