1aecf5604958f34117368b39f39cfef78af2b58e
Barshat Shrestha <sthabarshat@gmail.com>
2025-09-12 14:14:21 +0545

    edit

--- frontend/src/component/user/Layout.jsx
+++ frontend/src/component/user/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 import React from "react";
 import Header from "./Header";
-import Sidebar from "../Sidebar";
+import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 
 function Layout({ children }) {
