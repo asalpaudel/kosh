305cbd09b8582579d835bab3a075cba1d78daee1
yeloSidddd <siddhartharajgiri@gmail.com>
2025-11-07 22:31:02 +0545

    Added NetworkController and related backend models/repositories

---
+++ backend/src/main/java/com/kosh/backend/repository/NetworkRepository.java
@@ -0,0 +1,9 @@
+package com.kosh.backend.repository;
+
+import com.kosh.backend.model.Network;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NetworkRepository extends JpaRepository<Network, Long> {
+}
