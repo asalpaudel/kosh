2ad3bcbc3ba7b99e69a92bd104c9e26c5ae4ba56
Barshat Shrestha <sthabarshat@gmail.com>
2025-11-14 14:47:10 +0545

    cors error

--- backend/src/main/java/com/kosh/backend/controller/FinanceController.java
+++ backend/src/main/java/com/kosh/backend/controller/FinanceController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/finance")
-@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:5173" }, allowCredentials = "true")
+//@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:5173" }, allowCredentials = "true")
 public class FinanceController {
 
     @Autowired
